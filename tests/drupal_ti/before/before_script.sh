eef7a2bf5a6572b2bdfc06ffb5367f1b29410d9d
Talha Paracha <14besemparacha@seecs.edu.pk>
2016-08-03 02:13:53 +0500

    manually cloning all dependencies now

--- tests/drupal_ti/before/before_script.sh
+++ tests/drupal_ti/before/before_script.sh
@@ -11,5 +11,9 @@ set -e $DRUPAL_TI_DEBUG
 drupal_ti_ensure_drupal
 
 # Manually clone the dependencies
+cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
+git clone --branch 8.x-3.x https://git.drupal.org/project/encrypt.git
+git clone --branch 8.x-1.x https://git.drupal.org/project/key.git
+git clone --branch 8.x-1.x https://git.drupal.org/project/encrypt_seclib.git
 cd "$DRUPAL_TI_DRUPAL_DIR"
 composer require phpseclib/phpseclib:"^2.0.0"
